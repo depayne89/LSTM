41c5eb9a29f2c417adc51a51c945a88811d320c3
depayne89 <depayne89@gmail.com>
2020-04-07 13:57:45 +1000

    Dataset object for the whole dataset created

--- preprocessing.py
+++ preprocessing.py
@@ -99,24 +99,42 @@ def calc_drop(data):
     print()
 
 
-class NVDataset(Dataset):
+class WholeDataset(Dataset):
 
-    def __init__(self, iPt,  transform=None):
+    def __init__(self, iPt, train=True, train_percent=80, stepback=2,  transform=None):
         self.iPt = iPt
         self.transform = transform
-        self.rawDir = 'Patient_' + ds.get_patient(iPt)
+        self.train = train
+        self.train_percent = train_percent
+        self.stepback=stepback
+        self.rawDir = '/media/NVdata/Patient_' + ds.get_patient(iPt) + '/'
+        self.set = 'train'
+        if not train:
+            self.set = 'test'
+
+        f = h5py.File('/media/NVdata/SzTimes/all_train_2step_%d_%d.mat' % (train_percent, iPt), 'r')
+        self.labels = np.asarray(f['sample_labels'])
+        self.times = np.asarray(f['sample_times'])
+        f.close()
 
     def test(self):
-        print(self.rawDir)
+        print(self.labels)
 
     # def __len__(self):
     #     return self.len
 
-    # def __getitem__(self, idx):  # DATA doesn't have to be loaded until here!
-    #
-    #
-    #
-    #     return x, y
+    def __getitem__(self, idx):  # DATA doesn't have to be loaded until here!
 
-tester = NVDataset(1)
-tester.test()
+        y = self.labels[idx]
+        if y==2 or y==-1:
+            y=0
+
+        start = self.times[idx]
+        end = start + 600
+
+        x = get_data(self.iPt, start, end)
+
+        return x, y
+
+tester = WholeDataset(1)
+print(tester[0][0].shape)
